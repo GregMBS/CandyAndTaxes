c0d33ccbf9e5f68cea2f6c1f7e06a467a0929ef7
Greg Blumenthal <greg@gmbs-consulting.com>
2020-05-25 21:00:00 -0500

    budgetary

--- js/makeCandy.js
+++ js/makeCandy.js
@@ -34,6 +34,37 @@ function makeBox(n) {
     return box;
 }
 
+const distribution = [
+        {start: 28, regressive: 27, proportional: 21, progressive: 10, redistribution: 10},
+        {start: 13, regressive: 12, proportional: 10, progressive: 7, redistribution: 7},
+        {start: 11, regressive: 10, proportional: 8, progressive: 6, redistribution: 6},
+        {start: 8, regressive: 7, proportional: 6, progressive: 4, redistribution: 4},
+        {start: 8, regressive: 7, proportional: 6, progressive: 4, redistribution: 4},
+        {start: 7, regressive: 6, proportional: 5, progressive: 4, redistribution: 4},
+        {start: 5, regressive: 4, proportional: 4, progressive: 3, redistribution: 3},
+        {start: 5, regressive: 4, proportional: 4, progressive: 3, redistribution: 3},
+        {start: 4, regressive: 3, proportional: 3, progressive: 3, redistribution: 3},
+        {start: 3, regressive: 2, proportional: 2, progressive: 2, redistribution: 3},
+        {start: 3, regressive: 2, proportional: 2, progressive: 2, redistribution: 3},
+        {start: 2, regressive: 1, proportional: 2, progressive: 2, redistribution: 3},
+        {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 3},
+        {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 3},
+        {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 3},
+        {start: 0, regressive: 0, proportional: 0, progressive: 0, redistribution: 3}
+    ]
+;
+
+const government = [
+    {id: 'defense', start: 0, regressive: 6, proportional: 6, progressive: 6, redistribution: 6},
+    {id: 'education', start: 0, regressive: 2, proportional: 5, progressive: 5, redistribution: 5},
+    {id: 'infrastructure', start: 0, regressive: 2, proportional: 6, progressive: 7, redistribution: 7},
+    {id: 'healthcare', start: 0, regressive: 2, proportional: 6, progressive: 6, redistribution: 6},
+    {id: 'general', start: 0, regressive: 3, proportional: 5, progressive: 5, redistribution: 5},
+    {id: 'savings', start: 0, regressive: 0, proportional: 0, progressive: 18, redistribution: 6},
+    {id: 'total', start: 0, regressive: 15, proportional: 28, progressive: 47, redistribution: 35}
+]
+
+
 $(function () {
 
     const startButton = $('#start');
@@ -58,59 +89,63 @@ $(function () {
         });
     }
 
-    let distribution = [
-            {start: 28, regressive: 27, proportional: 21, progressive: 10, redistribution: 10},
-            {start: 13, regressive: 12, proportional: 10, progressive: 7, redistribution: 7},
-            {start: 11, regressive: 10, proportional: 8, progressive: 6, redistribution: 6},
-            {start: 8, regressive: 7, proportional: 6, progressive: 4, redistribution: 4},
-            {start: 8, regressive: 7, proportional: 6, progressive: 4, redistribution: 4},
-            {start: 7, regressive: 6, proportional: 5, progressive: 4, redistribution: 4},
-            {start: 5, regressive: 4, proportional: 4, progressive: 3, redistribution: 3},
-            {start: 5, regressive: 4, proportional: 4, progressive: 3, redistribution: 3},
-            {start: 4, regressive: 3, proportional: 3, progressive: 3, redistribution: 3},
-            {start: 3, regressive: 2, proportional: 2, progressive: 2, redistribution: 3},
-            {start: 3, regressive: 2, proportional: 2, progressive: 2, redistribution: 3},
-            {start: 2, regressive: 1, proportional: 2, progressive: 2, redistribution: 3},
-            {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 3},
-            {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 3},
-            {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 3},
-            {start: 0, regressive: 0, proportional: 0, progressive: 0, redistribution: 3}
-        ]
-    ;
+    function budget(taxation) {
+        const defense = government.find(lineItem => lineItem.id === 'defense')[taxation];
+        const education = government.find(lineItem => lineItem.id === 'education')[taxation];
+        const infrastructure = government.find(lineItem => lineItem.id === 'infrastructure')[taxation];
+        const healthcare = government.find(lineItem => lineItem.id === 'healthcare')[taxation];
+        const general = government.find(lineItem => lineItem.id === 'general')[taxation];
+        const savings = government.find(lineItem => lineItem.id === 'savings')[taxation];
+        const total = government.find(lineItem => lineItem.id === 'total')[taxation];
+        $('#defense').html(defense);
+        $('#education').html(education);
+        $('#infrastructure').html(infrastructure);
+        $('#healthcare').html(healthcare);
+        $('#general').html(general);
+        $('#savings').html(savings);
+        $('#total').html(total);
+    }
+
     distribution.sort(function () {
         return 0.5 - Math.random()
     });
     let result = distribution.map(({start}) => start);
     allHtml(result);
+    budget('start')
     fixButtons(startButton);
 
     startButton.click(function () {
         let result = distribution.map(({start}) => start);
         allHtml(result);
+        budget('start')
         fixButtons($(this));
     });
 
     $('#regressive').click(function () {
         let result = distribution.map(({regressive}) => regressive);
         allHtml(result);
+        budget('regressive')
         fixButtons($(this));
     });
 
     $('#proportional').click(function () {
         let result = distribution.map(({proportional}) => proportional);
         allHtml(result);
+        budget('proportional')
         fixButtons($(this));
     });
 
     $('#progressive').click(function () {
         let result = distribution.map(({progressive}) => progressive);
         allHtml(result);
+        budget('progressive')
         fixButtons($(this));
     });
 
     $('#redistribution').click(function () {
         let result = distribution.map(({redistribution}) => redistribution);
         allHtml(result);
+        budget('redistribution')
         fixButtons($(this));
     });
 
