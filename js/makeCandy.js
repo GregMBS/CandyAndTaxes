15fbd640f3f0ba41eec43fd23ba6496e6cd04594
Greg Blumenthal <greg@gmbs-consulting.com>
2020-06-08 21:25:54 -0500

    fix initial candy distribution

--- js/makeCandy.js
+++ js/makeCandy.js
@@ -45,22 +45,22 @@ function makeLineItem(n) {
 
 
 const distribution = [
-        {start: 28, regressive: 27, proportional: 21, progressive: 10, redistribution: 10},
-        {start: 13, regressive: 12, proportional: 10, progressive: 7, redistribution: 7},
-        {start: 11, regressive: 10, proportional: 8, progressive: 6, redistribution: 6},
-        {start: 8, regressive: 7, proportional: 6, progressive: 4, redistribution: 4},
-        {start: 8, regressive: 7, proportional: 6, progressive: 4, redistribution: 4},
-        {start: 7, regressive: 6, proportional: 5, progressive: 4, redistribution: 4},
-        {start: 5, regressive: 4, proportional: 4, progressive: 3, redistribution: 3},
-        {start: 5, regressive: 4, proportional: 4, progressive: 3, redistribution: 3},
-        {start: 4, regressive: 3, proportional: 3, progressive: 3, redistribution: 3},
-        {start: 3, regressive: 2, proportional: 2, progressive: 2, redistribution: 3},
-        {start: 3, regressive: 2, proportional: 2, progressive: 2, redistribution: 3},
-        {start: 2, regressive: 1, proportional: 2, progressive: 2, redistribution: 3},
-        {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 3},
-        {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 3},
-        {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 3},
-        {start: 0, regressive: 0, proportional: 0, progressive: 0, redistribution: 3}
+        {start: 28, regressive: 27, proportional: 21, progressive: 11, redistribution: 11},
+        {start: 13, regressive: 12, proportional: 10, progressive: 8, redistribution: 8},
+        {start: 10, regressive: 9, proportional: 8, progressive: 6, redistribution: 6},
+        {start: 8, regressive: 7, proportional: 6, progressive: 5, redistribution: 5},
+        {start: 8, regressive: 7, proportional: 6, progressive: 5, redistribution: 5},
+        {start: 8, regressive: 7, proportional: 6, progressive: 5, redistribution: 5},
+        {start: 5, regressive: 4, proportional: 4, progressive: 4, redistribution: 4},
+        {start: 5, regressive: 4, proportional: 4, progressive: 4, redistribution: 4},
+        {start: 4, regressive: 3, proportional: 3, progressive: 4, redistribution: 4},
+        {start: 3, regressive: 2, proportional: 2, progressive: 3, redistribution: 3},
+        {start: 3, regressive: 2, proportional: 2, progressive: 3, redistribution: 3},
+        {start: 2, regressive: 1, proportional: 1, progressive: 2, redistribution: 2},
+        {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 2},
+        {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 2},
+        {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 2},
+        {start: 0, regressive: 0, proportional: 0, progressive: 0, redistribution: 2}
     ]
 ;
 
