562f8c712bff3fb37b834b312b1acc26bf9d9bc6
Greg Blumenthal <greg@gmbs-consulting.com>
2020-06-09 14:06:57 -0500

    separate data

---
+++ js/candyData.js
@@ -0,0 +1,29 @@
+const distribution = [
+        {start: 28, regressive: 27, proportional: 21, progressive: 11, redistribution: 11},
+        {start: 13, regressive: 12, proportional: 10, progressive: 8, redistribution: 8},
+        {start: 10, regressive: 9, proportional: 8, progressive: 6, redistribution: 6},
+        {start: 8, regressive: 7, proportional: 6, progressive: 5, redistribution: 5},
+        {start: 8, regressive: 7, proportional: 6, progressive: 5, redistribution: 5},
+        {start: 8, regressive: 7, proportional: 6, progressive: 5, redistribution: 5},
+        {start: 5, regressive: 4, proportional: 4, progressive: 4, redistribution: 4},
+        {start: 5, regressive: 4, proportional: 4, progressive: 4, redistribution: 4},
+        {start: 4, regressive: 3, proportional: 3, progressive: 4, redistribution: 4},
+        {start: 3, regressive: 2, proportional: 2, progressive: 3, redistribution: 3},
+        {start: 3, regressive: 2, proportional: 2, progressive: 3, redistribution: 3},
+        {start: 2, regressive: 1, proportional: 1, progressive: 2, redistribution: 2},
+        {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 2},
+        {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 2},
+        {start: 1, regressive: 0, proportional: 0, progressive: 1, redistribution: 2},
+        {start: 0, regressive: 0, proportional: 0, progressive: 0, redistribution: 2}
+    ]
+;
+
+const government = [
+    {id: 'defense', start: 0, regressive: 6, proportional: 6, progressive: 6, redistribution: 6},
+    {id: 'education', start: 0, regressive: 2, proportional: 5, progressive: 5, redistribution: 5},
+    {id: 'infrastructure', start: 0, regressive: 2, proportional: 6, progressive: 7, redistribution: 7},
+    {id: 'healthcare', start: 0, regressive: 2, proportional: 6, progressive: 6, redistribution: 6},
+    {id: 'general', start: 0, regressive: 3, proportional: 5, progressive: 5, redistribution: 5},
+    {id: 'savings', start: 0, regressive: 0, proportional: 0, progressive: 18, redistribution: 6},
+    {id: 'total', start: 0, regressive: 15, proportional: 28, progressive: 47, redistribution: 35}
+]
